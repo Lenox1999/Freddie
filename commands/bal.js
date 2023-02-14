b4b284c8798abe100b45c0f97e15a1351195515c
Lenox <65367770+Lenox1999@users.noreply.github.com>
2023-02-15 00:06:46 +0100

    kleinen Rechtschreibfehler behoben

--- commands/bal.js
+++ commands/bal.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("bal")
-    .setDescription("Erfahre alles über deinen derzeitigen KontoStand"),
+    .setDescription("Erfahre alles über deinen derzeitigen Kontostand"),
 
   async execute(interaction, client) {
+    // !!! KANN BISHER NUR KONTOSTAND ABLESEN, ES FEHLT NOCH STREAK MULTIPLIER UND FÄHIGKEITEN !!!
+   
     const User = mongoose.models.User;
     User.findOne({ _id: interaction.member.id })
       .select("coinAmmount")

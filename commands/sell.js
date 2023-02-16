62ef78505e6d4c65dae91882b9fff45a47c2202d
Lenox <65367770+Lenox1999@users.noreply.github.com>
2023-02-16 17:33:57 +0100

    zum /sell Command die Wechselkurs Option hinzugefügt um nur den Wechselkurs einzusehen

--- commands/sell.js
+++ commands/sell.js
@@ -4,7 +4,16 @@ const mongoose = require("mongoose");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("sell")
-    .setDescription("Alle Burger verkaufen um Geld bekommen"),
+    .setDescription("Alle Burger verkaufen um Geld bekommen")
+    .addStringOption((option) =>
+      option
+        .setName("wunsch")
+        .setDescription("Verkaufen oder nur Wechselkurs einsehen?")
+        .addChoices(
+          { name: "Wechselkurs", value: "xchange" },
+          { name: "Verkaufen", value: "xsell" }
+        ).setRequired(true)
+    ),
   async execute(interaction, client) {
     const userId = interaction.member.id;
 
@@ -16,7 +25,7 @@ module.exports = {
       "burgerAmmount coinAmmount name"
     );
 
-    const exchange = await Exchange.findOne({_id: 'Exchange'}, 'value');
+    const exchange = await Exchange.findOne({ _id: "Exchange" }, "value");
     console.log(exchange);
 
     if (interaction.member.id === client.user.id) {
@@ -26,13 +35,40 @@ module.exports = {
       return;
     }
 
-    user.coinAmmount = Math.round(user.coinAmmount + (exchange.value * user.burgerAmmount));
-    user.burgerAmmount = 0;
+    if (interaction.options.getString("wunsch") === "xsell") {
+      user.coinAmmount = Math.round(
+        user.coinAmmount + exchange.value * user.burgerAmmount
+      );
+      user.burgerAmmount = 0;
 
-    user.save();
+      user.save();
 
-    interaction.reply(
-      `Erfolg! Du hast jetzt ${user.coinAmmount} Coins und ${user.burgerAmmount} Burger, ${user.name}`
-    );
+      let sellEmbed = new EmbedBuilder()
+        .setAuthor({
+          name: interaction.member.displayName,
+          iconURL: interaction.member.displayAvatarURL(),
+        })
+        .setTitle("ERFOLG ALLE COINS VERKAUFT!")
+        .setDescription(
+          `
+        Erfolg! Du hast jetzt ${user.coinAmmount} Coins und ${user.burgerAmmount} Burger, ${user.name}
+        Der derzeitige Wechselkurs beträgt ${exchange.value} 
+        `
+        );
+      interaction.reply({ embeds: [sellEmbed] });
+      return;
+    } else {
+      let showExchangeEmbed = new EmbedBuilder()
+        .setTitle(`Derzeitiger Wechselkurs von ${interaction.guild.name}`)
+        .setThumbnail(interaction.guild.iconURL())
+        .setDescription(
+          `
+          Der derzeitige Wechselkurs dieses Servers beträgt ${exchange.value}!
+          Das bedeutet ein Burger ist derzeit ${exchange.value} Coins wert!
+        `
+        );
+
+      interaction.reply({ embeds: [showExchangeEmbed] });
+    }
   },
 };
